6506fc36df2fd26269f90c1afc0b9412a4ec8d8d
Saad-Dupuy Jean-Christophe <saad.dupuy@yaahoo.fr>
2008-12-18 00:45:39 +0100

    Hop!

--- Java/src/ihmprojet2/Environnement/Terre.java
+++ Java/src/ihmprojet2/Environnement/Terre.java
@@ -10,7 +10,7 @@ public class Terre {
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
     // #[regen=yes,id=DCE.F112A1D2-92B0-1312-FD35-307A19AF497A]
     // </editor-fold> 
-    private bool engrais;
+    private Boolean engrais;
 
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
     // #[regen=yes,id=DCE.1CF8BBA5-170B-DCBD-C339-1C873B4D58CC]
@@ -21,14 +21,14 @@ public class Terre {
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
     // #[regen=yes,regenBody=yes,id=DCE.EA4D23E1-3B9A-C397-9E78-A43774C1F909]
     // </editor-fold> 
-    public bool isEngrais () {
+    public Boolean isEngrais () {
         return engrais;
     }
 
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
     // #[regen=yes,regenBody=yes,id=DCE.6BFA3C2B-390F-99F3-A363-656AD58B684E]
     // </editor-fold> 
-    public void setEngrais (bool val) {
+    public void setEngrais (Boolean val) {
         this.engrais = val;
     }
 
