763b1b5ab2f1ba6b7e09b94615abbf18979b3450
Geoffroy Carrier <geoffroy.carrier@koon.fr>
2008-12-19 20:26:04 +0100

    IHM en force !

---
+++ Java/src/ihmprojet2/Environnement/Climat.java
@@ -0,0 +1,54 @@
+package ihmprojet2.Environnement;
+
+
+// <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+// #[regen=yes,id=DCE.ABCA9A9D-A7E4-5AF9-5EFC-9891D0D4D5ED]
+// </editor-fold> 
+public class Climat {
+
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+    // #[regen=yes,id=DCE.EDE0451B-DE57-64AC-8734-F79DE59CE5E9]
+    // </editor-fold> 
+    private int temperature;
+
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+    // #[regen=yes,id=DCE.A2444577-2543-9771-0117-049981B294CF]
+    // </editor-fold> 
+    private int lumiere;
+
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+    // #[regen=yes,id=DCE.A1358131-78D2-9EEC-9A87-D52401C9C5CB]
+    // </editor-fold> 
+    public Climat () {
+    }
+
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+    // #[regen=yes,regenBody=yes,id=DCE.7F0BD958-6FB6-9FEB-557C-573584BBFD5E]
+    // </editor-fold> 
+    public int getLumiere () {
+        return lumiere;
+    }
+
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+    // #[regen=yes,regenBody=yes,id=DCE.18E081BC-DE17-8BC7-8C21-D464FDDFDB32]
+    // </editor-fold> 
+    public void setLumiere (int val) {
+        this.lumiere = val;
+    }
+
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+    // #[regen=yes,regenBody=yes,id=DCE.41AB5F67-A977-0792-6F1F-C0FD98BC9AF8]
+    // </editor-fold> 
+    public int getTemperature () {
+        return temperature;
+    }
+
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+    // #[regen=yes,regenBody=yes,id=DCE.081F35BF-B8D4-3128-B6F3-C7D595FDEB97]
+    // </editor-fold> 
+    public void setTemperature (int val) {
+        this.temperature = val;
+    }
+
+}
+
