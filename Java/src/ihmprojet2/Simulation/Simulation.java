ebcd1c7f48eef0aad4a4cffeca356de0bd52df47
Jean-Christophe Saad Dupuys <jc@snibbits.localdomain>
2008-12-22 18:10:06 +0100

    J'arrete de faire de la merde avec les versions...

--- Java/src/ihmprojet2/Simulation/Simulation.java
+++ Java/src/ihmprojet2/Simulation/Simulation.java
@@ -49,6 +49,7 @@ public class Simulation implements PropertyChangeListener, Serializable {
     }
 
     public Plante[] getEtatsPlante2() {
+
         return etatsPlante2;
     }
 
@@ -62,23 +63,21 @@ public class Simulation implements PropertyChangeListener, Serializable {
 
     public void setPlante1(TypePlante plante) throws Exception {
         plante1 = plante;
-        if (plante == TypePlante.SANS) {
-            propertyChangeSupport.firePropertyChange(PROP_ETATSPLANTE1, 0, -1);
-        } else {
+        if (plante != TypePlante.SANS) {
             updateEtatsPlante1();
             updateEtatsPlante2();
         }
+        propertyChangeSupport.firePropertyChange(PROP_ETATSPLANTE1, 0, -2);
     }
 
     public void setPlante2(TypePlante plante) throws Exception {
-        plante2 =plante;
-        
-        if (plante == TypePlante.SANS) {
-            propertyChangeSupport.firePropertyChange(PROP_ETATSPLANTE2, 0, -2);
-        } else {
+        plante2 = plante;
+
+        if (plante != TypePlante.SANS) {
             updateEtatsPlante1();
             updateEtatsPlante2();
         }
+        propertyChangeSupport.firePropertyChange(PROP_ETATSPLANTE2, 0, -2);
     }
 
     public Environnement getEnvironnment() {
@@ -99,32 +98,82 @@ public class Simulation implements PropertyChangeListener, Serializable {
                 break;
         }
         setEtatsPlante1(resultat);
-        propertyChangeSupport.firePropertyChange(PROP_ETATSPLANTE1, 0, 1);
     }
 
     public void updateEtatsPlante2() throws Exception {
+        System.out.println("update etat plante 2");
         Plante[] resultat = new Plante[7];
         environnement.setAutrePlante(plante1);
         switch (plante2) {
             case TOMATITO:
+                System.out.println("Tomatito");
                 Tomatito.setEnvironnement(environnement);
                 resultat = Tomatito.getEtats().clone();
                 break;
             case CACAI:
+                System.out.println("Cacai");
                 Cacai.setEnvironnement(environnement);
                 resultat = Cacai.getEtats().clone();
                 break;
         }
         setEtatsPlante2(resultat);
-        propertyChangeSupport.firePropertyChange(PROP_ETATSPLANTE2, 0, 2);
     }
 
     public synchronized void propertyChange(PropertyChangeEvent evt) {
         try {
-            updateEtatsPlante1();
-            updateEtatsPlante2();
+            if ((plante1 == TypePlante.CACAI) && (plante2 == TypePlante.TOMATITO)) {
+                environnement.getClimat().reduitLumiere();
+                updateEtatsPlante1();
+                environnement.getClimat().augmenteLumiere();
+                updateEtatsPlante2();
+            } else {
+                if ((plante1 == TypePlante.TOMATITO) && (plante2 == TypePlante.CACAI)) {
+                    environnement.getClimat().reduitLumiere();
+                    updateEtatsPlante2();
+                    environnement.getClimat().augmenteLumiere();
+                    updateEtatsPlante1();
+                } else {
+                    updateEtatsPlante1();
+                    updateEtatsPlante2();
+                }
+            }
+
         } catch (Exception ex) {
             Logger.getLogger(Simulation.class.getName()).log(Level.SEVERE, "PAAAM", ex);
+        }
+        propertyChangeSupport.firePropertyChange("tout", 0, 1);
+    }
+
+    public String getplante1LumiereDescription() throws Exception {
+        DegresLumiere d = environnement.getClimat().getLumiere();
+        boolean reduit = ((plante1 == TypePlante.CACAI) && (plante2 == TypePlante.TOMATITO));
+                
+        switch (d) {
+            case DIRECTE:
+                return reduit ? "indirecte" : "directe";
+            case INDIRECTE:
+                return reduit ? "obscurité" : "indirecte";
+            case OBSCURITE:
+                return "obscurité";
+            default:
+                return "indéterminée";
+
+        }
+    }
+
+    public String getplante2LumiereDescription() throws Exception {
+        DegresLumiere d = environnement.getClimat().getLumiere();
+        boolean reduit = ((plante2 == TypePlante.CACAI) && (plante1 == TypePlante.TOMATITO));
+        switch (d) {
+            case DIRECTE:
+                return reduit ? "indirecte" : "directe";
+            case INDIRECTE:
+                return reduit ? "obscurité" : "indirecte";
+            case OBSCURITE:
+                return "obscurité";
+            default:
+                return "indéterminée";
+
         }
     }
 
